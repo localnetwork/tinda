67c06824dcde9a0988198b5e9bbcf7ce63853b02
Clifford <c.iyac123@gmail.com>
2025-04-28 17:03:15 +0800

    chore: duplicate query

--- start/routes.ts
+++ start/routes.ts
@@ -63,11 +63,5 @@ router
   .use(middleware.auth())
   .prefix(prefix)
 
-router
-  .group(() => {
-    router.get('/seller-center/:uuid', [StoresController, 'sellerStoreIndex'])
-    router.get('/seller-center/:store_id/users', [UserStoresController, 'storeUsers'])
-  })
-
   .use(middleware.auth())
   .prefix(prefix)
